1a2d35e232e9b074252d2fc77126d18495e3a4bc
João Victor Kulka <jv.kulka13@gmail.com>
2022-12-29 16:14:24 -0300

    ToolTip em map
    Realizado a criação de uma Tooltip em cima do mapa para mostrar nova informação.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import navegacaoAnimacaoScroll from './modules/scroll-animacao.js'
 import navegacaoPerguntas from './modules/nav-perguntas.js'
 import navegacaoTab from './modules/nav-tabs.js'
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 navegacaoScrollSuave()
 navegacaoAnimacaoScroll()
 navegacaoPerguntas()
 navegacaoTab()
 initModal()
+initTooltip()
