b4facef1dbc14b8ff4a9cf22585b3916d5500072
João Victor Kulka <jv.kulka13@gmail.com>
2022-12-29 14:23:57 -0300

    Modal Login
    Realizado crição de função 'Modal' para login.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import navegacaoScrollSuave from './modules/scroll-suave.js'
 import navegacaoAnimacaoScroll from './modules/scroll-animacao.js'
 import navegacaoPerguntas from './modules/nav-perguntas.js'
 import navegacaoTab from './modules/nav-tabs.js'
+import initModal from './modules/modal.js'
 
 navegacaoScrollSuave()
 navegacaoAnimacaoScroll()
 navegacaoPerguntas()
 navegacaoTab()
+initModal()
