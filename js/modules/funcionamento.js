d8e4ab4334a04d7655c80a9142f0009a4047706e
João Victor Kulka <jv.kulka13@gmail.com>
2023-01-01 23:29:49 -0300

    Função horário de funcionamento
    Finalizado função de horário de funcionamento.

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,8 +1,24 @@
 export default function horarioFuncionamento() {
     const funcionamento = document.querySelector('[data-semana]')
-    const diasSemana = funcionamento.dataset.semana
+    const diasSemana = funcionamento.dataset.semana.split(',').map(Number)
+    const horarioSemana = funcionamento.dataset.horario.split(',').map(Number)
 
-    console.log(diasSemana)
+    const dataAgora = new Date()
+    const diaAgora = dataAgora.getDay()
+    const horarioAgora = dataAgora.getHours()
+
+    const semanaAberta = diasSemana.indexOf(diaAgora) !== -1
+
+    const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1])
+
+    if (semanaAberta && horarioAberto) {
+        funcionamento.classList.add('aberto')
+    } else {
+        funcionamento.classList.add('fechado')
+    }
+
+
+    console.log(semanaAberta)
 
 
 }
